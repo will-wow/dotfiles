58ad583fbbb5ab7890556a87b217bf60bdb64f28
Will Ockelmann-Wagner <will@willwow.dev>
2023-11-01 17:14:29 -0700

    fix firenvim, and toggle the sidebar harder

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Firenvim
+vim.g.firenvim_config = {
+  localSettings = {
+    [".*"] = {
+      takeover = "never", -- don't take over without a keypress
+      cmdline = "firenvim", -- don't use noice, it doesn't work in firenvim
+    },
+  },
+}
