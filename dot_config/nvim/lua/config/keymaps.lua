bc882f27cdaf1e47c45d7bd4f22fccddc95bdb26
Will Ockelmann-Wagner <will@willwow.dev>
2023-11-15 13:10:28 -0800

    fix tmux making vim move lines around

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remove Alt-hj, they conflict with tmux
+vim.keymap.del("n", "<A-k>")
+vim.keymap.del("n", "<A-j>")
